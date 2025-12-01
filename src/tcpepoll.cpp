6aad1f90039a8e6ab26e215caf04c0de1397977a
zymJDFH <2994059821@qq.com>
2025-12-01 11:27:27 +0800

    封装了TcpServer类

--- src/tcpepoll.cpp
+++ src/tcpepoll.cpp
@@ -6,30 +6,16 @@
 #include<unistd.h>
 #include<cstring>
 #include <netinet/tcp.h>  
-#include"InetAddress.h"
-#include "Socket.h"
-#include "Epoll.h"
 #include "EventLoop.h"
+#include "TcpServer.h"
 int main(int argc, char const *argv[])
 {
      if(argc!=3){
         std::cout<<"ip port"<<std::endl;
         return -1;
     }
-    InetAddress servaddr(argv[1],atoi(argv[2]));
-    Socket servsock(createnonblocking());
-    servsock.setkeepalive(true);
-    servsock.setreuseaddr(true);
-    servsock.setreuseport(true);
-    servsock.settcpnodelay(true);
-    servsock.bind(servaddr);
-    servsock.listen();
-
-    EventLoop loop;
-    Channel *servchannel =new Channel(&loop,servsock.fd());
-    servchannel->setreadcallback(std::bind(&Channel::newconnection,servchannel,&servsock));
-    servchannel->enablereading();
-
-    loop.run();
+    TcpServer tcpserver(argv[1],atoi(argv[2]));
+    tcpserver.start();
+    
     return 0;
 }
